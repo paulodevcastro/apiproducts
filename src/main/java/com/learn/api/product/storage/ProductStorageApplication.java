b44e34f523183e8795dd5313528fd2f5e33179c8
Yeti <paulogui702@gmail.com>
2024-01-18 18:27:36 -0300

    first commit

---
+++ src/main/java/com/learn/api/product/storage/ProductStorageApplication.java
@@ -0,0 +1,13 @@
+package com.learn.api.product.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/api/product/storage/ProductStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.api.product.storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
